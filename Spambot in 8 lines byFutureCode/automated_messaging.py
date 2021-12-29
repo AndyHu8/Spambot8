235cf731005c8ba1458d2644e8d40d48421193d9
胡龙 Hulong <47930486+AndyHu8@users.noreply.github.com>
2021-12-29 14:09:22 +0100

    Add files via upload

---
+++ Spambot in 8 lines byFutureCode/automated_messaging.py
@@ -0,0 +1,12 @@
+import time
+import pyautogui
+
+def SendMessage():
+    time.sleep(4)
+    text = open('message.txt')
+    
+    for line in text:
+        pyautogui.typewrite(line)
+        pyautogui.press('enter')
+
+SendMessage()
